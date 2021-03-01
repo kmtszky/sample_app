ecd90d6030414dedace7ac6a141069fa8e7c5d2c
EC2 Default User <ec2-user@ip-172-31-76-203.ec2.internal>
2021-03-01 06:05:07 +0000

    [Update] 2-5 Rails:todolists_control > indexアクションの定義

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,15 +1,21 @@
 class TodolistsController < ApplicationController
   def new
+    #データが単数のため[@list]となっている
     @list=List.new
   end
 
   def create
-    #データを新規登録するためのインスタンス
+    #データを新規登録するためのインスタンス。viewを介さないため@
     list = List.new(list_params)
     #データをデータベースへ保存するためのsaveメソッド
     list.save
     #top画面へリダイレクト
     redirect_to '/top'
+  end
+  
+  def index
+    #すべてのデータを取り込むため複数形[@lists]となっている
+    @lists=List.all
   end
 
   private
