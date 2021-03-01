20f4b70f4373974d27e652cd87b66fc138af9ec2
EC2 Default User <ec2-user@ip-172-31-76-203.ec2.internal>
2021-03-01 10:26:55 +0000

    [Fix] 2-6 Rails: routes.rb 9行目誤字修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get 'todolists' => 'todolists#index'
   # as: '名前' ：「名前付きルート」URLをわかりやすく書くために使われる。今回は、'todolists#show'の設定を、todolistとして利用できるの意。
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
-  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_tosolist'
+  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
 end
