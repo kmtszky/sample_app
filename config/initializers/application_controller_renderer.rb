c87fc9023ff5fcee53eae94c5d7f0ec135016cec
EC2 Default User <ec2-user@ip-172-31-76-203.ec2.internal>
2021-03-02 08:23:38 +0000

    [Finish] 2-7 Rails:シークレットキーの追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '538e8581a9a20c8572abec79e924d328ef0cf8c81f11347102d379518dc9d849deb362a6c6be88c2858be5e9b60beb1b3c9bef6cdc74012b0afcc6a145a9c930'
